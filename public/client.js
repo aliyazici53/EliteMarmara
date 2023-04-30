e3a9cc477d547887223f27ef277f702ba27826f0
ali yazici <aliyazici@ali-MacBook-Air.local>
2023-04-30 20:09:36 +0300

    first commit

---
+++ public/client.js
@@ -0,0 +1,51 @@
+const baseUrl = "http://127.0.0.1:3300/";
+
+let formInfo = {
+    'names': '',
+    'tel': '',
+    'email': '',
+    'guestNum': '',
+    'date': '',
+    'time': ''
+}
+
+function saveMail() {
+    var element = document.getElementById('mce-EMAIL');
+    saving(element.value);
+}
+
+async function saving(information) {
+    if (!information) {return};
+    
+    const res = await fetch(baseUrl + 'info', {
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({parcel : information})
+    });
+}
+
+
+function getForm() {
+    var fullName = document.getElementsByClassName('form-control');
+    var length = 6;
+    var num = 0;
+
+    Object.keys(formInfo).forEach(key => {
+        formInfo[key.toString()] = fullName[num].value;
+        num++;
+    })
+    sendInfo(formInfo);
+    for (let index = 0; index < length; index++) {
+        fullName[index].value = '';
+    }
+}
+
+async function sendInfo(information) {
+    if (!information) {return};
+    const res = await fetch(baseUrl, {
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({parcel: information})
+    });
+}
+
---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const port = 3300;
+
+var nodemailer = require('nodemailer');
+
+app.use(express.static('public'));
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'amzyaziciali1@gmail.com',
+      pass: 'wqallwswwfepbrwj'
+    }
+  });
+
+app.post('/', (req, res) => {
+    const {parcel} = req.body;      
+    var message = `
+    AD_SOYAD: ${parcel['names']}, 
+    TELEFON: ${parcel['tel']}, 
+    EMAİL: ${parcel['email']}, 
+    ZİYARETÇİ SAYISI: ${parcel['guestNum']},
+    TARİH: ${parcel['date']}, 
+    SAAT: ${parcel['time']}
+    `;
+
+    var mailDetails = {
+        from: 'amzyaziciali1@gmail.com',
+        to: 'amzyaziciali1@gmail.com',
+        subject: 'Rezervation Informations',
+        text: message
+    };
+
+    transporter.sendMail(mailDetails, function(err, data) {
+        if (err) {
+          console.log('An error occurred...', err);
+        } else {
+          console.log('Verification email was sent to your account');
+        }
+      });
+    }
+)
+
+
+app.post('/info', (req, res) => {
+  const {parcel} = req.body;
+  var message = `${parcel}`;
+
+
+  var mailDetails = {
+    from: 'amzyaziciali1@gmail.com',
+    to: 'amzyaziciali1@gmail.com',
+    subject: 'Mail Subscription Elite Marmara',
+    text: message
+  };
+
+  transporter.sendMail(mailDetails, function(err, data) {
+    if (err) {
+      console.log('An error occurred...', err);
+    } else {
+      console.log('Verification email was sent to your account');
+    }
+  });
+})
+
+
+app.listen(port, () => {
+    console.log(`Server has started on port: ${port}`);
+});
